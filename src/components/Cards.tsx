6c6561740b743f3cca89e15884517b1f5905d4e2
cbolson <chris@cbolson.com>
2023-04-23 22:16:24 +0200

    fix - links to demos

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,13 +3,21 @@ function Cards({ title, img, imgAlt, urlDemo, urlGithub }) {
     <div className='card'>
       <div className='card__img'>
         <picture>
-          <source srcSet={img} type="image/webp" />
-          <source srcSet={imgAlt} type="image/png" />
+          <source srcSet={img} type='image/webp' />
+          <source srcSet={imgAlt} type='image/png' />
           <img src={img} />
         </picture>
-        </div>
+      </div>
       <h3 className='card__title'>{title}</h3>
       <div className='card__links'>{urlDemo}</div>
+      <div className='card__links'>
+        <a href={urlDemo} className='btn' target='_blank'>
+          Demo
+        </a>
+        <a href={urlGithub} className='btn' target='_blank'>
+          Github
+        </a>
+      </div>
     </div>
   );
 }
